2f01166176b695417b15a88501acc84a697a2db7
mikebrown24 <mikebrownn@hotmail.com>
2017-07-11 16:54:31 +0000

    fully done

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -1,9 +1,5 @@
 class DojosController < ApplicationController
   def index
-    @branch = params[:branch]
-    @street = params[:street]
-    @city = params[:city]
-    @state = params[:state]
     @dojos = Dojo.all
   end
   
